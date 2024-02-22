0a481240fc32f166fa5807b993da99e89a4e83a5
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-02-22 16:05:33 +0100

    notjing is done made

--- Assets/Scripts/P2_Flyweight/TreeSpawner.cs
+++ Assets/Scripts/P2_Flyweight/TreeSpawner.cs
@@ -7,25 +7,35 @@ using Random = UnityEngine.Random;
 
 public class TreeSpawner : MonoBehaviour
 {
+
+    private TreeSeasonColors _treeSeasonColors;
+    private ColorInfo colorInfo;
     public Tree TreePrefab;
     private float _currentCooldown;
     
     const float _totalCooldown = 0.2f;
+
+   
     
     void FixedUpdate()
     {
+       
         this._currentCooldown -= Time.deltaTime;
         if (this._currentCooldown <= 0f)
         {
             this._currentCooldown += _totalCooldown;
             SpawnTree();
         }
+       
     }
 
     void SpawnTree()
     {
+        
         var randomPositionX = Random.Range(-6f, 6f);
         var randomPositionY = Random.Range(-6f, 6f);
         Instantiate(this.TreePrefab, new Vector2(randomPositionX, randomPositionY), Quaternion.identity);
+        // pass treeSeason to tree
+
     }
 }
